2672540bcc0734c9405cba4d29d105fc383d3aae
roopamcz <87115511+roopamcz@users.noreply.github.com>
2023-03-24 22:39:24 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Devylawyer\'s blog' });
+  res.render('index', { title: 'Hello World' });
 });
 
 export default router;
