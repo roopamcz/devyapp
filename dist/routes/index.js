06f12a1ae915b29018570a5a75d1c5499f84b21e
roopamcz <87115511+roopamcz@users.noreply.github.com>
2023-03-24 23:14:55 +0530

    Update index.js

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -14,7 +14,7 @@ var router = _express2.default.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  res.render('index', { title: 'Hello World Folks' });
 });
 
 exports.default = router;
