17cecd5509de598dcea6535c9a160eade901022a
roopamcz <87115511+roopamcz@users.noreply.github.com>
2023-03-24 22:12:03 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "healthandresourcerequirement"
-#     key    = "client/rapid-innovation/us-east-2.tfstate"
-#     region = "us-east-2"
-#     dynamodb_table  = "terraform-locks"
-#   }
-# }
